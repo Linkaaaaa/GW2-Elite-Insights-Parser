f518d49d58f4497a35cea7faa856be472ce1b45a
EliphasNUIT <bichon.jekfer@gmail.com>
2026-03-31 16:18:05 +0200

    update html

--- GW2EIBuilders/HtmlModels/HtmlActors/TargetDto.cs
+++ GW2EIBuilders/HtmlModels/HtmlActors/TargetDto.cs
@@ -13,6 +13,8 @@ internal class TargetDto : ActorDto
     public double BarrierLeftPercent { get; set; }
     public int BarrierLeft { get; set; }
 
+    public List<double[]> HpBars { get; set; }
+
     public TargetDto(SingleActor target, ParsedEvtcLog log, ActorDetailsDto details) : base(target, log, details)
     {
         HbHeight = target.HitboxHeight;
@@ -37,7 +39,34 @@ internal class TargetDto : ActorDto
                 BarrierLeftPercent = barrierUpdates.Last().Value;
             }
         }
-        HpLeft = target.GetCurrentHealth(log, HpLeftPercent);
         BarrierLeft = target.GetCurrentBarrier(log, BarrierLeftPercent, log.LogData.LogEnd);
+        var healthBars = target.GetHealthBars();
+        if (healthBars != null)
+        {
+            Health = 0;
+            HpBars = new(healthBars.Count);
+            bool activeFound = false;
+            foreach (var bar in healthBars)
+            {
+                Health += (long)(bar.hpValue * (bar.maxPercent - bar.minPercent) / 100);
+                var behaviorValue = 0;
+                if (bar.active)
+                {
+                    activeFound = true;
+                    behaviorValue = 1;
+                    HpLeft = (int)(bar.hpValue * HpLeftPercent / 100);
+                } 
+                else if (activeFound)
+                {
+                    behaviorValue = 2;
+                    HpLeft += (int)(bar.hpValue * (bar.maxPercent - bar.minPercent) / 100);
+                }
+                HpBars.Add([bar.minPercent, bar.maxPercent, bar.hpValue, behaviorValue]);
+            }
+        } 
+        else
+        {
+            HpLeft = target.GetCurrentHealth(log, HpLeftPercent);
+        }
     }
 }
