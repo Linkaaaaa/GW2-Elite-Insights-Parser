b8e45274a2177b03643e88c3f3d44334e6c6599e
EliphasNUIT <bichon.jekfer@gmail.com>
2026-04-01 18:31:27 +0200

    make spec checking englobed agent item compatible + spec utility APIs

--- GW2EIEvtcParser/EIData/ProfHelpers/ProfHelper.cs
+++ GW2EIEvtcParser/EIData/ProfHelpers/ProfHelper.cs
@@ -813,7 +813,7 @@ internal static class ProfHelper
 
     public static void AdjustMinionName(AgentItem minion)
     {
-        switch (minion.GetFinalMaster().BaseSpec)
+        switch (minion.GetFinalMaster().GetBaseSpecAtTime(minion.HalfAware))
         {
             case Spec.Mesmer:
                 MesmerHelper.AdjustMinionName(minion);
@@ -821,7 +821,7 @@ internal static class ProfHelper
             default:
                 break;
         }
-        switch (minion.GetFinalMaster().Spec)
+        switch (minion.GetFinalMaster().GetSpecAtTime(minion.HalfAware))
         {
             case Spec.Evoker:
                 EvokerHelper.AdjustMinionName(minion);
