cd4a73839c2fc220d8dc9f6bb9e5fddac8a5e038
EliphasNUIT <bichon.jekfer@gmail.com>
2026-05-06 09:38:10 +0300

    added JsonWvWMapData, only contains shard and team ids for now

---
+++ GW2EIBuilders/JsonModels/JsonWvWMapDataBuilder.cs
@@ -0,0 +1,28 @@
+﻿using GW2EIEvtcParser;
+using GW2EIEvtcParser.EIData;
+using GW2EIEvtcParser.ParsedData;
+using GW2EIJSON;
+
+namespace GW2EIBuilders.JsonModels;
+
+internal static class JsonWvWMapDataBuilder
+{
+    public static JsonWvWMapData BuildJsonWvWMapData(ParsedEvtcLog log, WvWTeamsEvent wvwTeamsEvent,  HashSet<ulong> teampMap)
+    {
+        var jsonWvWMapData = new JsonWvWMapData
+        {
+            BlueShardID = wvwTeamsEvent.BlueShardID,
+            RedShardID = wvwTeamsEvent.RedShardID,
+            GreenShardID = wvwTeamsEvent.GreenShardID,
+
+            BlueTeamID = wvwTeamsEvent.BlueTeamID,
+            GreenTeamID = wvwTeamsEvent.GreenTeamID,
+            RedTeamID = wvwTeamsEvent.RedTeamID,
+        };
+
+        teampMap.UnionWith([wvwTeamsEvent.BlueTeamID, wvwTeamsEvent.GreenTeamID, wvwTeamsEvent.RedTeamID]);
+
+        return jsonWvWMapData;
+    }
+
+}
